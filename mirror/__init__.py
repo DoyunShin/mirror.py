f8d48fc2a4a7e7931b9de640bf59101ca1aed74e
DoyunShin <doyun.shin@gmail.com>
2024-02-07 17:50:02 +0900

    fix duplicated codes

--- mirror/__init__.py
+++ mirror/__init__.py
@@ -25,28 +25,3 @@ import mirror.plugin
 
 mirror.sync.load_default()
 #mirror.plugin.plugin_loader()
-
-from pathlib import Path
-import logging
-
-conf: mirror.structure.Config
-packages: mirror.structure.Packages
-confPath: Path
-publishPath: Path
-logger: logging.Logger
-debug: bool
-worker: dict[str, mirror.structure.Worker]
-__version__: str
-
-import mirror.toolbox
-import mirror.event
-import mirror.sync
-import mirror.logger
-import mirror.command
-#import mirror.socket
-import mirror.config
-import mirror.plugin
-
-
-mirror.sync.load_default()
-#mirror.plugin.plugin_loader()
